1a99e9689280b8e96d2ab821e5e36b989a982ee5
shibaramdas-git <shibaram.daseng@gmail.com>
2024-06-06 14:43:53 +0530

    Loading spinner added, evcery 10sec  of revalidation cached data, delaying 2s of fetching to show loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center mt-[70px]">
+      <img src="spinning-dots.svg" alt="Loading......." className="h-[100px]" />
+    </div>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,36 @@
 const OMDB_KEY = process.env.OMDB_KEY;
 export default async function HomePage({ searchParams }) {
   const genre = searchParams.genre || "movie";
-  const res = await fetch(
-    `http://www.omdbapi.com/?s=adventure&type=${genre}&apikey=${OMDB_KEY}j`
-  );
+  const res = await new Promise((resolve) => {
+    setTimeout(async () => {
+      const response = await fetch(
+        `http://www.omdbapi.com/?s=adventure&type=${genre}&apikey=${OMDB_KEY}`,
+        { next: { revalidate: 10 } }
+      );
+      resolve(response);
+    }, 2000); //Delay of 2 seconds
+  });
+
+  // const res = await fetch(
+  //   `http://www.omdbapi.com/?s=adventure&type=${genre}&apikey=${OMDB_KEY}`,
+  //   { next: { revalidate: 20 } }
+  // );
   const data = await res.json();
+  const movies = data.Search;
   console.log(data);
   if (data.Response == "False") {
     throw new Error("OOPS!! Failed to fetch data from database..");
   }
-  return <div>-----------Good morning Home page------------</div>;
+  return (
+    <div>
+      {data &&
+        movies.map((movie) => (
+          <div key={movie.imdbID}>
+            <h3>{movie.Title}</h3>
+            <p>Year : {movie.Year}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
+// {next: {revalidate: 20}} in fetch method for cashing after 20sec
